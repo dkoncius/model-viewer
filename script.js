5b5e5d1046eacab28d24a6e4afe1635e1832503b
dkoncius <as.koncius@gmail.com>
2023-09-29 14:56:16 +0300

    slides and ar button

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modelViewer = document.querySelector("model-viewer");
+
+    // Upload model functionality
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            modelViewer.onload = () => {
+                URL.revokeObjectURL(modelViewer.src);
+            }
+            modelViewer.src = URL.createObjectURL(this.files[0]);
+        }
+    });
+
+    // Set modelViewer dimensions
+    modelViewer.style.width = "100%";
+    modelViewer.style.height = "100vh";
+
+    // Choose model functionality
+    window.switchSrc = (element, name) => {
+        modelViewer.src = `./glb/${name}.glb`;
+        modelViewer.poster = `./slider/${name}.webp`;
+        const slides = document.querySelectorAll(".slide");
+        slides.forEach((slide) => slide.classList.remove("selected"));
+        element.classList.add("selected");
+    };
+
+    // Slider interaction
+    document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
+        ev.preventDefault();
+    });
+});
