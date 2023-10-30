492b69351e07e3337e0e34adf5ad686864d518e0
dkoncius <as.koncius@gmail.com>
2023-10-30 13:23:26 +0200

    init

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const modelViewer = document.querySelector("model-viewer");
- 
- // Set modelViewer dimensions
- modelViewer.style.width = "100%";
- modelViewer.style.height = "100vh";
+
+// Set modelViewer dimensions
+modelViewer.style.width = "100%";
+modelViewer.style.height = "100vh";
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Upload model functionality
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]) {
-            modelViewer.onload = () => {
-                URL.revokeObjectURL(modelViewer.src);
-            }
-            modelViewer.src = URL.createObjectURL(this.files[0]);
-        }
-    });
+  // Upload model functionality
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+      modelViewer.onload = () => {
+        URL.revokeObjectURL(modelViewer.src);
+      }
+      modelViewer.src = URL.createObjectURL(this.files[0]);
+    }
+  });
 
-    // Choose model functionality
-    window.switchSrc = (element, name) => {
-        modelViewer.src = `./glb/${name}.glb`;
-        modelViewer.poster = `./slider/${name}.webp`;
-        const slides = document.querySelectorAll(".slide");
-        slides.forEach((slide) => slide.classList.remove("selected"));
-        element.classList.add("selected");
-    };
+  // Choose model functionality
+  window.switchSrc = (element, name) => {
+    modelViewer.src = `./glb/${name}.glb`;
+    modelViewer.poster = `./slider/${name}.webp`;
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((slide) => slide.classList.remove("selected"));
+    element.classList.add("selected");
+  };
 
-    // Slider interaction
-    document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
-        ev.preventDefault();
-    });
+  // Slider interaction
+  document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
+    ev.preventDefault();
+  });
 });
